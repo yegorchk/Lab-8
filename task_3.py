a5b41f7ea2a72986cf6420e8fa7b34e65ec2e10c
Yegor R <german14122000@gmail.com>
2025-04-12 01:34:05 +0300

    lab-8_v2

--- task_3.py
+++ task_3.py
@@ -1,10 +1,10 @@
-import time
 import cv2
 
 def video_processing():
-    cap = cv2.VideoCapture(1)
     last_box = None
-    i = 0
+    fly = cv2.imread('fly64.png')
+    print(fly.shape)
+    cap = cv2.VideoCapture(1)
     while True:
         ret, frame = cap.read()
         if not ret:
@@ -18,13 +18,19 @@ def video_processing():
         contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         if contours:
             largest_contour = max(contours, key=cv2.contourArea)
-            if cv2.contourArea(largest_contour) > 50:
+            if cv2.contourArea(largest_contour) > 100:
                 x, y, w, h = cv2.boundingRect(largest_contour)
-                last_box = (x, y, w, h)
+                center_width = (x + (w//2))
+                center_height = (y + (h//2))
+                last_box = (center_width, center_height)
 
         if last_box:
-            x, y, w, h = last_box
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            center_width, center_height = last_box
+            x1 = int(center_width - 32)
+            x2 = int(center_width + 32)
+            y1 = int(center_height - 32)
+            y2 = int(center_height + 32)
+            frame[y1:y2, x1:x2] = fly
 
         cv2.imshow('frame with box', frame)
         cv2.imshow('edges', edges)
@@ -38,3 +44,4 @@ def video_processing():
 
 if __name__ == '__main__':
     video_processing()
+    
